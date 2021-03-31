3bf0375a91287fa56d4659c8a26b5ff942fbfa10
elwandyTD <eltirder0@gmail.com>
2021-03-31 21:14:44 +0700

    fix page transaction and product

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,25 +1,192 @@
 import React, { Component } from "react";
-import { Card, Row, Col, Button } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
+import axios from "axios";
+import {
+  Card,
+  Row,
+  Col,
+  Button,
+  Space,
+  Table,
+  Divider,
+  Input,
+  Modal,
+} from "antd";
+import {
+  PlusSquareFilled,
+  DeleteFilled,
+  EditFilled,
+  ExclamationCircleFilled,
+} from "@ant-design/icons";
+
 import CardHeader from "../components/CardHeader";
 
+const { confirm } = Modal;
+const { REACT_APP_API_HOST } = process.env;
+
 class Product extends Component {
+  state = {
+    products: [],
+    column: [
+      {
+        title: "Kode",
+        dataIndex: "kode",
+        key: "kode",
+        width: 100,
+      },
+      {
+        title: "Name",
+        dataIndex: "nama",
+        key: "nama",
+        width: 200,
+      },
+      {
+        title: "Category",
+        dataIndex: "title",
+        key: "title",
+        width: 120,
+      },
+      {
+        title: "Price",
+        dataIndex: "harga",
+        key: "harga",
+      },
+      {
+        title: "Action",
+        key: "action",
+        render: (record) => (
+          <Space>
+            <Button
+              disabled
+              onClick={() => this.showModalEdit(record)}
+              type="primary"
+              icon={<EditFilled />}
+            />
+            <Button
+              type="danger"
+              onClick={() => this.deleteItem(record)}
+              icon={<DeleteFilled />}
+            />
+          </Space>
+        ),
+      },
+    ],
+  };
+
+  getProducts = () => {
+    axios
+      .get(`${REACT_APP_API_HOST}/item`)
+      .then(({ data }) => {
+        if (data.success) {
+          this.setState({
+            products: data.data,
+          });
+        } else {
+          console.log(data);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  deleteItem = (item) => {
+    const reload = this.getProducts;
+    confirm({
+      centered: true,
+      title: "Delete product !",
+      icon: <ExclamationCircleFilled />,
+      content: "Are you sure gonna delete this product ?",
+      onOk() {
+        return axios
+          .delete(`${REACT_APP_API_HOST}/item/${item.kode}`)
+          .then(({ data }) => {
+            if (data.success) {
+              reload();
+            } else {
+              console.log(data);
+            }
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
+  };
+
+  onChangeValueNew = (e, name) => {
+    this.setState({});
+  };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
   render() {
+    const { product, type } = this.state;
+
     return (
       <Card className="style__card">
         <CardHeader title="Product" desc="Manage products that you have" />
+        <Divider orientation="left" plain>
+          Manage products
+        </Divider>
+        <Button
+          onClick={this.showModalInsert}
+          style={{ marginBottom: "15px" }}
+          type="primary"
+          icon={<PlusSquareFilled />}
+        >
+          Add new product
+        </Button>
         <Row>
-          <Col xs={24} sm={24} md={24} lg={8} xl={8} xxl={8}>
-            <Button size="small" type="primary" icon={<SearchOutlined />}>
-              Search
-            </Button>
-          </Col>
           <Col>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+            <Table
+              scroll={{ x: 700 }}
+              dataSource={this.state.products}
+              columns={this.state.column}
+              rowKey={(record) => record.kode}
+              footer={() => (
+                <p style={{ fontSize: "16px", textAlign: "right" }}>
+                  You have: <b>{this.state.products.length}</b> product
+                </p>
+              )}
+              pagination={false}
+            />
           </Col>
         </Row>
+        <Modal
+          title={type === "edit" ? "Update product" : "Add new product"}
+          visible={this.state.visibleModal}
+          footer={null}
+          onCancel={this.cancelModalEdit}
+        >
+          <span>Title product</span>
+          <Input
+            placeholder="Title"
+            value={
+              type === "edit"
+                ? product && product.title && product.title
+                : this.state.title
+            }
+            onChangeCapture={(value) =>
+              type === "edit"
+                ? this.onChangeValue(value)
+                : this.onChangeTitle(value)
+            }
+            required={true}
+          />
+          <Button
+            style={{ marginTop: "15px", textAlign: "right" }}
+            type="primary"
+            loading={this.state.loading}
+            onClick={this.okModal}
+          >
+            Save
+          </Button>
+        </Modal>
       </Card>
     );
   }
--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -1,8 +1,166 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { Card, Row, Col, Button, Space, Table, Divider, Modal } from "antd";
+import {
+  // PlusSquareFilled,
+  DeleteFilled,
+  // EditFilled,
+  ExclamationCircleFilled,
+} from "@ant-design/icons";
+
+import CardHeader from "../components/CardHeader";
+
+const { confirm } = Modal;
+const { REACT_APP_API_HOST } = process.env;
 
 class Transaction extends Component {
+  state = {
+    transactions: [],
+    column: [
+      {
+        title: "Nota",
+        dataIndex: "id_nota",
+        key: "id_nota",
+        width: 100,
+      },
+      {
+        title: "Kode Pelanggan",
+        dataIndex: "kode_pelanggan",
+        key: "kode_pelanggan",
+        width: 200,
+      },
+      {
+        title: "Subtotal",
+        dataIndex: "subtotal",
+        key: "subtotal",
+        width: 120,
+      },
+      {
+        title: "Tanggal",
+        dataIndex: "tgl",
+        key: "tgl",
+      },
+      {
+        title: "Action",
+        key: "action",
+        render: (record) => (
+          <Space>
+            <Button
+              type="danger"
+              onClick={() => this.deleteItem(record)}
+              icon={<DeleteFilled />}
+            />
+          </Space>
+        ),
+      },
+    ],
+  };
+
+  deleteItem = (item) => {
+    const reload = this.getTransactions;
+    confirm({
+      centered: true,
+      title: "Delete transaction !",
+      icon: <ExclamationCircleFilled />,
+      content: "Are you sure gonna delete this transaction ?",
+      onOk() {
+        return axios
+          .delete(`${REACT_APP_API_HOST}/transaction/${item.id_nota}`)
+          .then(({ data }) => {
+            if (data.success) {
+              reload();
+            } else {
+              console.log(data);
+            }
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
+  };
+
+  getTransactions = () => {
+    axios
+      .get(`${REACT_APP_API_HOST}/transaction`)
+      .then(({ data }) => {
+        if (data.success) {
+          this.setState({
+            transactions: data.data,
+          });
+        } else {
+          console.log(data);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  componentDidMount() {
+    this.getTransactions();
+  }
+
   render() {
-    return <h1>Transaction</h1>;
+    // const { type } = this.state;
+
+    return (
+      <Card className="style__card">
+        <CardHeader title="Transaction" desc="Manage customer transaction" />
+        <Divider orientation="left" plain>
+          Manage transaction
+        </Divider>
+        <Row>
+          <Col>
+            <Table
+              scroll={{ x: 700 }}
+              dataSource={this.state.transactions}
+              columns={this.state.column}
+              rowKey={(record) => record.id_nota}
+              footer={() => (
+                <p style={{ fontSize: "16px", textAlign: "right" }}>
+                  You have: <b>{this.state.transactions.length}</b> transaction
+                </p>
+              )}
+              pagination={false}
+            />
+          </Col>
+        </Row>
+        {/* <Modal
+          title={type === "edit" ? "Update product" : "Add new product"}
+          visible={this.state.visibleModal}
+          footer={null}
+          onCancel={this.cancelModalEdit}
+        >
+          <span>Title product</span>
+          <Input
+            placeholder="Title"
+            value={
+              type === "edit"
+                ? product && product.title && product.title
+                : this.state.title
+            }
+            onChangeCapture={(value) =>
+              type === "edit"
+                ? this.onChangeValue(value)
+                : this.onChangeTitle(value)
+            }
+            required={true}
+          />
+          <Button
+            style={{ marginTop: "15px", textAlign: "right" }}
+            type="primary"
+            loading={this.state.loading}
+            onClick={this.okModal}
+          >
+            Save
+          </Button>
+        </Modal> */}
+      </Card>
+    );
   }
 }
 
